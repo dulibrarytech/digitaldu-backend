dbc87cd7ace7c4fb40d653d49fc045a676485047
Fernando <fernando.reyes@du.edu>
2018-06-25 09:08:33 -0600

    updated api routes

--- repository/routes.js
+++ repository/routes.js
@@ -5,24 +5,29 @@ var Repo = require('../repository/controller');
 // TODO: apply api security.  i.e. API key (for discovery layer)
 module.exports = function (app) {
 
-
-    /* Used by discovery layer*/
-    app.route('/api/objects')
+    //--- Used by discovery layer ---//
+    /*  */
+    app.route('/api/v1/objects')
         .get(Repo.get_objects);
 
-    app.route('/api/object')
+    /* Gets single object (Collection or Object) */
+    app.route('/api/v1/object')
         .get(Repo.get_object);
 
 
     /* collections used by discovery layer and repo */
     /* gets top level collections */
+    /*
     app.route('/api/collections')
         .get(Repo.get_collections);
+    */
 
     /* retrieves single collection record for edit used by repo*/
+    /*
     app.route('/api/collection')
         .get(Repo.get_collection)
         .put(Repo.update_collection);
+    */
 
     /*
     app.route('/api/collection/name')
@@ -30,21 +35,30 @@ module.exports = function (app) {
     */
 
     // used by discovery layer and repo
+    /*
     app.route('/api/collection/tn')
         .get(Repo.get_collection_tn);
     // .put(Repo.update_collection_tn);
+    */
 
+    /*
     app.route('/api/object/metadata')
         .get(Repo.get_object_metadata);
+    */
 
     // used by discovery layer
+    /*
     app.route('/api/object/tn')
         .get(Repo.get_object_tn);
+    */
 
+    /*
     app.route('/api/object/mods')
         .get(Repo.get_mods);
+    */
 
     // used by discovery layer
+    /*
     app.route('/api/object/image/jpg')
         .get(Repo.get_image_jpg);
 
@@ -62,4 +76,5 @@ module.exports = function (app) {
 
     app.route('/api/search')
         .get(Repo.do_search);
+    */
 };
