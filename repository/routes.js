15fde55a7270b161032dbe2dd961bb3993e0eea7
Fernando <fernando.reyes@du.edu>
2018-06-26 14:44:10 -0600

    cleaned up repository routes, controller, and model

--- repository/routes.js
+++ repository/routes.js
@@ -21,66 +21,7 @@ module.exports = function (app) {
     app.route('/api/admin/v1/object')
         .get(Repo.get_admin_object);
 
-
-    /* collections used by discovery layer and repo */
-    /* gets top level collections */
     /*
-    app.route('/api/collections')
-        .get(Repo.get_collections);
-    */
-
-    /* retrieves single collection record for edit used by repo*/
-    /*
-    app.route('/api/collection')
-        .get(Repo.get_collection)
-        .put(Repo.update_collection);
-    */
-
-    /*
-    app.route('/api/collection/name')
-        .get(Repo.get_collection_name);
-    */
-
-    // used by discovery layer and repo
-    /*
-    app.route('/api/collection/tn')
-        .get(Repo.get_collection_tn);
-    // .put(Repo.update_collection_tn);
-    */
-
-    /*
-    app.route('/api/object/metadata')
-        .get(Repo.get_object_metadata);
-    */
-
-    // used by discovery layer
-    /*
-    app.route('/api/object/tn')
-        .get(Repo.get_object_tn);
-    */
-
-    /*
-    app.route('/api/object/mods')
-        .get(Repo.get_mods);
-    */
-
-    // used by discovery layer
-    /*
-    app.route('/api/object/image/jpg')
-        .get(Repo.get_image_jpg);
-
-    app.route('/api/object/image/tiff')
-        .get(Repo.get_image_tiff);
-
-    app.route('/api/object/image/jp2')
-        .get(Repo.get_image_jp2);
-
-    app.route('/api/object/pdf')
-        .get(Repo.get_pdf);
-
-    app.route('/api/object/video/mp4')
-        .get(Repo.get_video_mp4);
-
     app.route('/api/search')
         .get(Repo.do_search);
     */
