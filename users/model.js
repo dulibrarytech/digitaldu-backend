89a9985d127459d04edf6ea1db3051736d3de5eb
Fernando <fernando.reyes@du.edu>
2019-04-22 13:36:39 -0600

    added error logging to user model

--- users/model.js
+++ users/model.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var fs = require('fs'),
-    config = require('../config/config'),
-    async = require('async'),
+const config = require('../config/config'),
+    fs = require('fs'),
+    logger = require('../libs/log4'),
     knex = require('knex')({
         client: 'mysql2',
         connection: {
@@ -13,6 +13,12 @@ var fs = require('fs'),
         }
     });
 
+/**
+ * Gets all users
+ * @param req
+ * @param callback
+ * @returns {boolean}
+ */
 exports.get_users = function (req, callback) {
 
     if (req.query.id !== undefined) {
@@ -43,92 +49,19 @@ exports.get_users = function (req, callback) {
             });
         })
         .catch(function (error) {
-            // TODO: add error callback
-            console.log(error);
+            logger.module().error('ERROR: unable to get users ' + error);
+            throw 'ERROR: unable to get users ' + error;
         });
-
-    /*
-    knex('tbl_users_tbl_groups')
-            .join('tbl_users', 'tbl_users.id', '=', 'tbl_users_tbl_groups.user_id')
-            .groupBy('tbl_users.id')
-            .select(
-            'tbl_users.id',
-            'tbl_users.du_id',
-            'tbl_users.email',
-            'tbl_users.first_name',
-            'tbl_users.last_name',
-            'tbl_users.status',
-            'tbl_users.created',
-            'tbl_users_tbl_groups.group_id'
-        )
-            .then(function (data) {
-                console.log(data);
-                callback({
-                    status: 200,
-                    content_type: {'Content-Type': 'application/json'},
-                    data: data,
-                    message: 'Users retrieved.'
-                });
-            })
-            .catch(function (error) {
-                // TODO: add error callback
-                console.log(error);
-            });
-
-        */
-    /*
-
-    async.waterfall([
-        getUsers,
-        getGroups
-    ], function (err, results) {
-        console.log(results);
-        callback({
-            status: 200,
-            content_type: {'Content-Type': 'application/json'},
-            data: results,
-            message: 'Counts retrieved.'
-        });
-    });
-    */
-
-    /*
-    knex('tbl_groups')
-        .join('tbl_users_tbl_groups', 'tbl_groups.id', '=', 'tbl_users_tbl_groups.group_id')
-        .join('tbl_users', 'tbl_users.id', '=', 'tbl_users_tbl_groups.user_id')
-        .select(
-            'tbl_users.id',
-            'tbl_users.du_id',
-            'tbl_users.email',
-            'tbl_users.first_name',
-            'tbl_users.last_name',
-            'tbl_users.status',
-            'tbl_users.created',
-            'tbl_users_tbl_groups.group_id',
-            'tbl_groups.group_name',
-            'tbl_groups.group_description',
-            'tbl_groups.permissions'
-        )
-        // .where('tbl_users.id', 1)
-        // .groupBy('tbl_groups.group_name')
-        .then(function (data) {
-            callback({
-                status: 200,
-                content_type: {'Content-Type': 'application/json'},
-                data: data,
-                message: 'Users retrieved.'
-            });
-        })
-        .catch(function (error) {
-            // TODO: add error callback
-            console.log(error);
-        });
-        */
 };
 
-var get_user = function (req, callback) {
+/**
+ * Gets one user
+ * @param req
+ * @param callback
+ */
+const get_user = function (req, callback) {
 
-    var id = req.query.id; // TODO: sanitize
+    let id = req.query.id; // TODO: sanitize
 
     knex('tbl_users')
         .select('id', 'du_id', 'email', 'first_name', 'last_name', 'status', 'created')
@@ -144,16 +77,19 @@ var get_user = function (req, callback) {
             });
         })
         .catch(function (error) {
-            // TODO: add error callback
-            console.log(error);
+            logger.module().error('ERROR: unable to get user ' + error);
+            throw 'ERROR: unable to get user ' + error;
         });
 };
 
-// checks whether user is allowed to access repo
+/**
+ * Checks user access
+ * @param username
+ * @param callback
+ */
 exports.check_auth_user = function (username, callback) {
 
     knex('tbl_users')
-        // .count('du_id as count')
         .select('id')
         .where({
             du_id: username,
@@ -174,15 +110,18 @@ exports.check_auth_user = function (username, callback) {
             }
         })
         .catch(function (error) {
-            // TODO: add error callback
-            console.log(error);
+            logger.module().error('ERROR: unable to check auth ' + error);
+            throw 'ERROR: unable to check auth ' + error;
         });
 };
 
-/* gets user information used when requesting repo data and objects */
+/**
+ * Gets user data
+ * @param username
+ * @param callback
+ */
 exports.get_auth_user_data = function (username, callback) {
 
-    // TODO: get group_id(s)
     knex('tbl_users')
         .select('id', 'du_id', 'email', 'first_name', 'last_name')
         .where({
@@ -190,8 +129,6 @@ exports.get_auth_user_data = function (username, callback) {
             status: 1
         })
         .then(function (data) {
-
-            console.log(data);
 
             if (data.length === 1) {
                 callback({
@@ -204,19 +141,28 @@ exports.get_auth_user_data = function (username, callback) {
             }
         })
         .catch(function (error) {
-            // TODO: add error callback
-            console.log(error);
+            logger.module().error('ERROR: unable to get user data ' + error);
+            throw 'ERROR: unable to get user data ' + error;
         });
 };
 
+/**
+ * Updates user data
+ * @param req
+ * @param callback
+ */
 exports.update_user = function (req, callback) {
     // TODO:...
 };
 
+/**
+ * Saves user data
+ * @param req
+ * @param callback
+ */
 exports.save_user = function (req, callback) {
 
-    // TODO: sanitize
-    var userObj = req.body;
+    let userObj = req.body;
 
     knex('tbl_users')
         .insert(userObj)
@@ -228,42 +174,12 @@ exports.save_user = function (req, callback) {
             });
         })
         .catch(function (error) {
-            console.log(error);
+            logger.module().error('ERROR: unable to get user data ' + error);
+
             callback({
                 status: 500,
                 content_type: {'Content-Type': 'application/json'},
                 message: 'Database error occurred.'
             });
-        });
-};
-
-exports.get_user_groups = function (req, callback) {
-
-    // TODO: sanitize
-    var id = req.query.id;
-
-    knex('tbl_users_tbl_groups')
-        .join('tbl_groups', 'tbl_groups.id', '=', 'tbl_users_tbl_groups.group_id')
-        .select(
-        'tbl_users_tbl_groups.group_id',
-        'tbl_groups.group_name',
-        'tbl_groups.group_description',
-        'tbl_groups.permissions',
-        'tbl_groups.resources'
-    )
-        .where({
-            user_id: id
-        })
-        .then(function (data) {
-            callback({
-                status: 200,
-                content_type: {'Content-Type': 'application/json'},
-                data: data,
-                message: 'User groups retrieved.'
-            });
-        })
-        .catch(function (error) {
-            // TODO: add error callback
-            console.log(error);
         });
 };
