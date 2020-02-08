e6f54d105e320ec4d7b478834000687b8b674f09
Fernando <fernando.reyes@du.edu>
2020-02-08 11:51:00 -0700

    cleaned up and documented stats module

--- public/app/stats.module.js
+++ public/app/stats.module.js
@@ -21,50 +21,75 @@ const statsModule = (function () {
     'use strict';
 
     let obj = {};
-
-    const renderError = function (message) {
-        $('#message').html(message);
-    };
-
     let api = configModule.getApi();
 
+    /**
+     * Renders repository stats on home page
+     * @param data
+     */
     const renderStats = function (data) {
 
-        $('#published-collection-count').html(data.published_collection_count.toLocaleString('en'));
-        $('#total-collection-count').html(data.total_collection_count.toLocaleString('en'));
-        $('#published-object-count').html(data.published_object_count.toLocaleString('en'));
-        $('#total-object-count').html(data.total_object_count.toLocaleString('en'));
-        $('#total-image-count').html(data.total_image_count.toLocaleString('en'));
-        $('#total-pdf-count').html(data.total_pdf_count.toLocaleString('en'));
-        $('#total-audio-count').html(data.total_audio_count.toLocaleString('en'));
-        $('#total-video-count').html(data.total_video_count.toLocaleString('en'));
+        document.querySelector('#published-collection-count').innerHTML = DOMPurify.sanitize(data.published_collection_count.toLocaleString('en'));
+        document.querySelector('#total-collection-count').innerHTML = DOMPurify.sanitize(data.total_collection_count.toLocaleString('en'));
+        document.querySelector('#published-object-count').innerHTML = DOMPurify.sanitize(data.published_object_count.toLocaleString('en'));
+        document.querySelector('#total-object-count').innerHTML = DOMPurify.sanitize(data.total_object_count.toLocaleString('en'));
+        document.querySelector('#total-image-count').innerHTML = DOMPurify.sanitize(data.total_image_count.toLocaleString('en'));
+        document.querySelector('#total-pdf-count').innerHTML = DOMPurify.sanitize(data.total_pdf_count.toLocaleString('en'));
+        document.querySelector('#total-audio-count').innerHTML = DOMPurify.sanitize(data.total_audio_count.toLocaleString('en'));
+        document.querySelector('#total-video-count').innerHTML = DOMPurify.sanitize(data.total_video_count.toLocaleString('en'));
 
-        $('#loading-published-collection-count').html('');
-        $('#loading-total-collection-count').html('');
-        $('#loading-published-object-count').html('');
-        $('#loading-total-object-count').html('');
-        $('#loading-image-count').html('');
-        $('#loading-pdf-count').html('');
-        $('#loading-audio-count').html('');
-        $('#loading-video-count').html('');
-        $('a').tooltip();
+        // clear loading... messages
+        document.querySelector('#loading-published-collection-count').innerHTML = '';
+        document.querySelector('#loading-total-collection-count').innerHTML = '';
+        document.querySelector('#loading-published-object-count').innerHTML = '';
+        document.querySelector('#loading-total-object-count').innerHTML = '';
+        document.querySelector('#loading-image-count').innerHTML = '';
+        document.querySelector('#loading-pdf-count').innerHTML = '';
+        document.querySelector('#loading-audio-count').innerHTML = '';
+        document.querySelector('#loading-video-count').innerHTML = '';
     };
 
+    /**
+     * Gets stats from repository
+     */
     obj.getStats = function () {
 
-        userModule.setHeaderUserToken();
-
-        $.ajax(api + '/api/admin/v1/stats')
-            .done(function(data) {
-                renderStats(data);
-            })
-            .fail(function(jqXHR, textStatus) {
-
-                if (jqXHR.status !== 200) {
-                    let message = '<div class="alert alert-danger"><i class="fa fa-exclamation-circle"></i> Error: (HTTP status ' + jqXHR.status + '. Unable to retrieve repository statistics.</div>';
-                    renderError(message);
+        let token = userModule.getUserToken();
+        let url = api + '/api/admin/v1/stats',
+            request = new Request(url, {
+                method: 'GET',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'x-access-token': token
                 }
             });
+
+        const callback = function (response) {
+
+            if (response.status === 200) {
+
+                response.json().then(function (data) {
+
+                    document.querySelector('#message').innerHTML = '';
+
+                    if (data.length === 0) {
+                        let message = '<div class="alert alert-info"><i class="fa fa-exclamation-circle"></i> No records found.</div>';
+                        document.querySelector('#message').innerHTML = message;
+                    } else {
+                        renderStats(data);
+                    }
+                });
+
+            } else {
+
+                let message = '<div class="alert alert-danger"><i class="fa fa-exclamation-circle"></i> Error: (HTTP status ' + DOMPurify.sanitize(response.status) + '. Unable to retrieve repository statistics.</div>';
+                helperModule.renderError(message);
+            }
+
+        };
+
+        http.req(request, callback);
     };
 
     obj.init = function () {
