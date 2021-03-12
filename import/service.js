9ad15d26255e0c2b185553513772684d1fcd716f
freyes <freyes@MacBook-Pro.local>
2021-03-12 15:13:44 -0700

    removed unused code from import service

--- import/service.js
+++ import/service.js
@@ -20,7 +20,6 @@
 
 const ARCHIVESSPACE = require('../libs/archivespace'),
     HTTP = require('../libs/http'),
-    SERVICE = require('../import/service'),
     LOGGER = require('../libs/log4');
 
 /**
